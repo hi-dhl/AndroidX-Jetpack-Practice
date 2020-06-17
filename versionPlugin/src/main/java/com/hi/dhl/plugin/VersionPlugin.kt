0ce17dafa460d0cb777927550155490077454e41
hi-dhl <hihuilong@gmail.com>
2020-06-17 15:33:49 +0800

    style: optimize VersionPlugin

--- versionPlugin/src/main/java/com/hi/dhl/plugin/VersionPlugin.kt
+++ versionPlugin/src/main/java/com/hi/dhl/plugin/VersionPlugin.kt
@@ -10,7 +10,7 @@ import org.gradle.api.Project
  *     desc  : 如果出现红色警告可以忽略，不会影响项目的编译和运行
  * </pre>
  */
-class Deps : Plugin<Project> {
+class VersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
     }
 

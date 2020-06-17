b6df466ccbd7331a2a72f65ca53d9093d2faadde
hi-dhl <hihuilong@gmail.com>
2020-06-17 11:54:29 +0800

    feat: add versionPlugin

---
+++ versionPlugin/src/main/java/com/hi/dhl/plugin/Deps.kt
@@ -0,0 +1,21 @@
+package com.hi.dhl.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2020/5/29
+ *     desc  : 如果出现红色警告可以忽略，不会影响项目的编译和运行
+ * </pre>
+ */
+class Deps : Plugin<Project> {
+    override fun apply(project: Project) {
+    }
+
+    companion object {
+
+
+    }
+}
---
+++ versionPlugin/src/test/kotlin/com/hi/dhl/plugin/DepsTest.java
@@ -0,0 +1,12 @@
+package com.hi.dhl.plugin;
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2020/5/29
+ *     desc  :
+ * </pre>
+ */
+public class DepsTest {
+
+}
